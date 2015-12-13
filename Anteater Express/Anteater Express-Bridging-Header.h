a196d820739d54c9a303705adabbe67dfcbfbd6e
ericshively <eshkidd@gmail.com>
2015-12-13 02:45:09 -0800

    empty News TabBarController and Swift Bridging Headers

---
+++ Anteater Express/Anteater Express-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  Created by Eric Shively on 12/12/15.
+//
+
+#import "AppDelegate.h"
+#import "AllRouteUpdatesViewController.h"
+#import "MapViewController.h"
+#import "MenuInfo.h"
+#import "BannerItemInfo.h"
+#import "LineInfo.h"
+#import "ItemInfo.h"
+#import "AEMenuBannerTableViewCell.h"
+#import "AEMenuFreeLineTableViewCell.h"
+#import "AEMenuPaidLineTableViewCell.h"
+#import "AEMenuItemTableViewCell.h"
+#import "AESideMenuTableViewController.h"
---
+++ Anteater Express/NewsAndAboutTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  NewsAndAboutTabBarController.swift
+//  Anteater Express
+//
+//  Created by Eric Shively on 12/12/15.
+//
+//
+
+import UIKit
+
+class NewsAndAboutTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        //print("hello")
+    }
+}
